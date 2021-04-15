417773f1ec5eb43024c648ab43873b24331420d2
Gurpreet Singh <gurubahre2019@gmail.com>
2021-04-15 15:03:32 -0700

    Changing theme button onclick

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ const themeChange = document.querySelector('#dark')
 themeChange.addEventListener('click', () =>{
     document.body.classList.toggle('dark')
 })
+function theme_button(){
+    
+    if (document.getElementById("dark").innerHTML==="dark_mode"){
+        document.getElementById("dark").innerHTML="light_mode";
+    }
+    else {
+        document.getElementById("dark").innerHTML="dark_mode";
+    }
+
+}
+    
