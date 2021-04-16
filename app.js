f467aff800658d60b7f88a4d12be6936af19e034
Gurpreet Singh <gurubahre2019@gmail.com>
2021-04-16 13:50:18 -0700

    Displaying notes in main

--- app.js
+++ app.js
@@ -57,9 +57,13 @@ function cleanUp(){
     const div =document.querySelector('#note-area')
     div.remove()
 }
-const notearray=[{title : "note1", body:"this is bost"}]
+const noteArray=[{title : "note1", body:"this is bost"}]
+
+const noteList=document.querySelector('#notes-list')
+
     
-window.addEventListener('load', createNoteList(notearray))
+window.addEventListener('load', createNoteList(noteArray))
+
 
 function noteListTemplate(title){
     return html=`
@@ -67,9 +71,38 @@ function noteListTemplate(title){
 
 }
 
+function displayNoteTemplate(note){
+    const{title,body}=note
+    return html=`
+    <div id="note">
+    <h2>${title}</h2>
+    <p>${body}</p>
+    <button onclick="removeNote()">close note</button>
+    </div>
+    `
+    
+}
+
+
 function createNoteList(arr){
     const noteDiv=document.querySelector('#notes-list')
     for (const note of arr){
         noteDiv.insertAdjacentHTML('beforeend',noteListTemplate(note.title))
     }
 }
+
+function displayNote(title, arr){
+    return note = arr.find(n=>n.title===title)
+}
+
+function removeNote(){
+    const note = document.querySelector('#note')
+    note.remove()
+}
+noteList.addEventListener('click', (e) =>{
+    const noteDisplayArea= document.querySelector('#note-display')
+    const title=e.target.textContent
+    const note = displayNote(title, noteArray)
+    noteDisplayArea.insertAdjacentHTML('beforeend', displayNoteTemplate(note))
+
+})
