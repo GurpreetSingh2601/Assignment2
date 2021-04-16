84d396b75c6dd3d031c45b6ab31462b089710f52
Gurpreet Singh <gurubahre2019@gmail.com>
2021-04-16 12:48:48 -0700

    Adding div area for user input

--- app.js
+++ app.js
@@ -13,4 +13,48 @@ function theme_button(){
     }
 
 }
+const notesArray=[]
+
+function defineNoteWritingSection(){
+    html= `
+    <div id ="note-area">
+    <div id ="note" contenteditable ="true"></div>
+    <button onclick="saveNote()">save</button>
+    <button onclick="cleanUp()">cancel</button>
+    </div>`
+    return html
+}
+function createContentWritingArea(){
+    const div= document.querySelector('#content')
+    div.insertAdjacentHTML('beforeend', defineNoteWritingSection())
+    setCursor()
+}
+function setCursor(){
+    const div=document.querySelector('#note')
+    div.focus()
+}
+function saveNote(){
+    const div=document.querySelector('#note')
+    const title=div.firstChild.textContent
+    const body=convertDivstoString()
+    notesArray.push(createNote(title,body))
+    cleanUp()
+}
+function createNote(title,body){
+    return {title,body}
+}
+
+function convertDivstoString(){
+    let str=""
+    const divs=[...document.querySelectorAll('[contenteditable] > div:not(:first-child)')]
+    for (const i of divs){
+        str += `${i.textContent}\n`
+    }
+    return str
+}
+
+function cleanUp(){
+    const div =document.querySelector('#note-area')
+    div.remove()
+}
     
